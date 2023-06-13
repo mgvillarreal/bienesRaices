325e6eeb161bb70bc92712a5166ebe0c778362fd
Maria Gabriela <mgvillarreal09@gmail.com>
2023-06-12 22:23:25 -0300

    first commit

---
+++ blog.php
@@ -0,0 +1,95 @@
+<?php
+    include './includes/templates/header.php';
+?>
+
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Nuestro Blog</h1>
+
+        <article class="entrada-blog">
+            <div class="imagen">
+                <picture>
+                    <source srcset="build/img/blog1.webp" type="image/webp">
+                    <source srcset="build/img/blog1.jpg" type="image/jpeg">
+                    <img src="build/img/blog1.jpg" alt="Texto entrada Blog">
+                </picture>
+            </div>
+
+            <div class="texto-entrada">
+                <a href="entrada.html">
+                    <h4>Terraza en el Techo de tu Casa</h4>
+                    <p class="informacion-meta">Escrito el: <span>20/05/2023</span> Por: <span>Admin</span></p>
+
+                    <p>
+                        Consejos para construir una terraza en el techo de tu casa con los mejores materiales y ahorrando dinero.
+                    </p>
+                </a>
+            </div>
+        </article>
+
+        <article class="entrada-blog">
+            <div class="imagen">
+                <picture>
+                    <source srcset="build/img/blog3.webp" type="image/webp">
+                    <source srcset="build/img/blog3.jpg" type="image/jpeg">
+                    <img src="build/img/blog3.jpg" alt="Texto entrada Blog">
+                </picture>
+            </div>
+
+            <div class="texto-entrada">
+                <a href="entrada.html">
+                    <h4>Construye una Piscina en tu Hogar</h4>
+                    <p class="informacion-meta">Escrito el: <span>07/05/2023</span> Por: <span>Admin</span></p>
+
+                    <p>
+                        Maximiza el espacio de tu hogar con esta guía, aprende a combinar muebles y colores para darle vida s tu espacio.
+                    </p>
+                </a>
+            </div>
+        </article>
+
+        <article class="entrada-blog">
+            <div class="imagen">
+                <picture>
+                    <source srcset="build/img/blog1.webp" type="image/webp">
+                    <source srcset="build/img/blog1.jpg" type="image/jpeg">
+                    <img src="build/img/blog1.jpg" alt="Texto entrada Blog">
+                </picture>
+            </div>
+
+            <div class="texto-entrada">
+                <a href="entrada.html">
+                    <h4>Guía para la Decoración de tu Hogar</h4>
+                    <p class="informacion-meta">Escrito el: <span>26/04/2023</span> Por: <span>Admin</span></p>
+
+                    <p>
+                        Maximiza el espacio de tu hogar con esta guía, aprende a combinar muebles y colores para darle vida s tu espacio.
+                    </p>
+                </a>
+            </div>
+        </article>
+
+        <article class="entrada-blog">
+            <div class="imagen">
+                <picture>
+                    <source srcset="build/img/blog4.webp" type="image/webp">
+                    <source srcset="build/img/blog4.jpg" type="image/jpeg">
+                    <img src="build/img/blog4.jpg" alt="Texto entrada Blog">
+                </picture>
+            </div>
+
+            <div class="texto-entrada">
+                <a href="entrada.html">
+                    <h4>Guía para la Decoración de tu Habitación</h4>
+                    <p class="informacion-meta">Escrito el: <span>12/04/2023</span> Por: <span>Admin</span></p>
+
+                    <p>
+                        Maximiza el espacio de tu hogar con esta guía, aprende a combinar muebles y colores para darle vida s tu espacio.
+                    </p>
+                </a>
+            </div>
+        </article>
+    </main>
+
+<?php
+    include './includes/templates/footer.php';
+?>
---
+++ nosotros.php
@@ -0,0 +1,52 @@
+<?php
+    include './includes/templates/header.php';
+?>
+
+    <main class="contenedor seccion">
+        <h1>Conoce sobre Nosotros</h1>
+
+        <section class="nosotros-contenido">
+            <div class="imagen">
+                <picture>
+                    <source srcset="build/img/nosotros.webp" type="image/webp">
+                    <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                    <img src="build/img/nosotros.jpg" alt="Sobre Nosotros">
+                </picture>
+            </div>
+
+            <div class="texto-nosotros">
+                <h4>25 Años de Experiencia</h4>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quia est repellat iste eius delectus fugiat reiciendis temporibus consequuntur mollitia ex quaerat, saepe adipisci hic, ut, dolorum cupiditate. Maiores, nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. A deleniti perferendis neque optio, perspiciatis est distinctio accusamus itaque temporibus, voluptates placeat quisquam repellendus molestias libero reiciendis possimus quam omnis magni.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam incidunt maxime autem perferendis sint delectus, illum sequi perspiciatis molestiae esse quos, est excepturi harum doloremque. Laboriosam alias illum facere adipisci?</p>
+            </div>
+        </section>
+    </main>
+
+    <section class="contenedor seccion">
+        <h1>Más Sobre Nosotros</h1>
+
+        <div class="iconos-nosotros">
+            <div class="icono">
+                <img src="build/img/icono1.svg" alt="Icono Seguridad" loading="lazy">
+                <h3>Seguridad</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita pariatur, provident, blanditiis corrupti, quia dolorum doloremque nobis quos iusto sed eveniet consequuntur earum eius eligendi labore laudantium obcaecati qui!</p>
+            </div>
+
+            <div class="icono">
+                <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
+                <h3>Precio</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita pariatur, provident, blanditiis corrupti, quia dolorum doloremque nobis quos iusto sed eveniet consequuntur earum eius eligendi labore laudantium obcaecati qui!</p>
+            </div>
+
+            <div class="icono">
+                <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
+                <h3>Tiempo</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita pariatur, provident, blanditiis corrupti, quia dolorum doloremque nobis quos iusto sed eveniet consequuntur earum eius eligendi labore laudantium obcaecati qui!</p>
+            </div>
+        </div> <!--iconos-nosotros-->
+    </section>
+
+<?php
+    include './includes/templates/footer.php';
+?>
